613baab372c2abf0d3a6cc276b2482297a39401f
Earth2509 <pattharapon.kijjanukij@gmail.com>
2026-08-12 22:19:54 +0700

    feat: seed request categories idempotently

---
+++ server/prisma/seed.ts
@@ -0,0 +1,22 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+const categoryNames = ["Account and Access", "Hardware", "Software", "Network"];
+
+async function main() {
+  for (const name of categoryNames) {
+    await prisma.category.upsert({
+      where: { name },
+      update: {},
+      create: { name },
+    });
+  }
+}
+
+main()
+  .then(() => prisma.$disconnect())
+  .catch(async (error) => {
+    console.error(error);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
